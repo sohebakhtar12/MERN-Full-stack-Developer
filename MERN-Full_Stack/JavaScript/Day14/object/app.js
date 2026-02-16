26690514828d85300b49da8bdbd0dccf6b4c4972
Soheb Akhtar <126268043+sohebakhtar12@users.noreply.github.com>
2026-02-16 14:05:56 +0530

    objectday14

---
+++ MERN-Full_Stack/JavaScript/Day14/object/app.js
@@ -0,0 +1,10 @@
+let str = "hello";
+console.log(str.length);
+console.log(str.charAt(4));
+console.log(str[2]);
+
+
+let str1 = "rupak"
+for (let i = 0; i < str1.length; i++) {
+    console.log(str1[str1.length - 1 - i]);
+}
